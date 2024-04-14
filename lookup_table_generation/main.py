481201c7788c96c5d0c399fe74f9363b3a3d7423
Roddeman, JA <julian.roddeman@rabobank.nl>
2024-04-14 12:33:43 +0200

    code

--- lookup_table_generation/main.py
+++ lookup_table_generation/main.py
@@ -39,11 +39,11 @@ def group_rows(df, mean_columns, min_matches, precision, save_csv=False):
         df[["group_code"] + mean_columns].groupby("group_code").mean().reset_index()
     )
     if save_csv:
-        # grouped train
+
         df.to_csv("grouped_train.csv", index=False)
-        # save lookup
+
         lookup.to_csv("lookup.csv", index=False)
-        # save params 
+
         with open("params.txt", "w") as f:
             f.write(f"min_matches: {min_matches}\n")
             f.write(f"precision: {precision}\n")
@@ -53,8 +53,8 @@ def group_rows(df, mean_columns, min_matches, precision, save_csv=False):
 def run_varied_params():
 
     results = []
-    for precision in range(1, 16):  # precision from 1 to 15
-        for min_matches in range(4, 7):  # min_matches from 4 to 6
+    for precision in range(1, 16):
+        for min_matches in range(4, 7):
             lookup = group_rows(train_csv.copy(), mean_columns, min_matches, precision)
             results.append((precision, min_matches, len(lookup)))
 
@@ -62,13 +62,12 @@ def run_varied_params():
 
 
 def plot_results(results, optimal_count=200):
-    # Prepare data
+
     precisions, min_matches, counts = zip(*results)
     precisions = np.array(precisions)
     min_matches = np.array(min_matches)
     counts = np.array(counts)
 
-    # Create a scatter plot colored by the number of groups
     plt.figure(figsize=(10, 6))
     scatter = plt.scatter(
         precisions,
@@ -81,21 +80,16 @@ def plot_results(results, optimal_count=200):
     )
     plt.colorbar(scatter, label="Number of Groups in Lookup Table")
 
-    # Set axis labels and title
     plt.xlabel("Precision")
     plt.ylabel("Minimum Matches")
     plt.title("Distribution of Group Counts in Lookup Table")
 
-    # Adjust y-axis to show only whole numbers
     plt.yticks(np.arange(min(min_matches), max(min_matches) + 1, 1))
     plt.ylim(min(min_matches) - 0.5, max(min_matches) + 0.5)
-    # Add grid for better readability
+
     plt.grid(True, which="both", linestyle="--", linewidth=0.5)
 
-    # Find the combination closest to 200 groups and mark it
-    optimal_index = np.abs(
-        counts - optimal_count
-    ).argmin()  # Index of the closest value to optimal_count
+    optimal_index = np.abs(counts - optimal_count).argmin()
     optimal_precision = precisions[optimal_index]
     optimal_min_matches = min_matches[optimal_index]
     plt.scatter(
@@ -108,7 +102,6 @@ def plot_results(results, optimal_count=200):
     )
     plt.legend()
 
-    # Save the plot
     plt.savefig("images/lookup_distribution.png")
 
     optimal_params = {
@@ -118,7 +111,6 @@ def plot_results(results, optimal_count=200):
     return optimal_params
 
 
-# Example usage:
 results = run_varied_params()
 optimal_params = plot_results(results, optimal_count=300)
 
