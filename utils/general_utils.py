4bbf86eb999b392a61c0cdb8dcb0039566b4a8b3
tabrom <tabrom@web.de>
2024-05-10 16:47:25 +0200

    add functionality to ensure dir structure

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -1,3 +1,6 @@
+import os 
+
+
 def update_config_samples(config, mode, length):
     if 'N_SAMPLES' not in dir(config):
         config.N_SAMPLES = dict()
@@ -8,3 +11,10 @@ def update_config_samples(config, mode, length):
     config.N_STEPS_PER_EPOCH[mode] = (config.N_SAMPLES[mode] // config.BATCH_SIZE)
     config.N_STEPS[mode] = config.N_STEPS_PER_EPOCH[mode] * config.N_EPOCHS + 1
     return config
+
+
+def check_dirs(config):
+    if config.log_dir not in os.listdir(): 
+        os.mkdir(config.logdir)
+    elif config.checkpoint_save_dir not in os.listdir(): 
+        os.mkdir(config.checkpoint_save_dir)
